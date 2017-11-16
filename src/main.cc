44be917267468e91e4b2e59cf51d9b0c4d820e6b
Sam Walker <walkersc@cs.utexas.edu>
2017-11-16 13:12:08 -0600

    initial commit.

---
+++ src/main.cc
@@ -0,0 +1,21 @@
+#include <GL/glew.h>
+#include <dirent.h>
+
+#include <image.h>
+
+#include <algorithm>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include <glm/gtx/component_wise.hpp>
+#include <glm/gtx/rotate_vector.hpp>
+#include <glm/gtx/string_cast.hpp>
+#include <glm/gtx/io.hpp>
+#include <debuggl.h>
+
+int main(int argc, char* argv[])
+{
+	return 0;
+}
