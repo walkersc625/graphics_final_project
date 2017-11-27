692cd8b45463577acf7c7b9b9a77caea83e5b582
Sam Walker <walkersc@cs.utexas.edu>
2017-11-27 14:53:29 -0600

    made a bunch of structs and functions.

--- src/Texture.cc
+++ src/Texture.cc
@@ -15,3 +15,65 @@ Texture::Texture(const string filepath)
 		printf("\timage height: %d\n", image.height);
 	}
 }
+
+Synth::Synth(Texture t)
+{
+	sample.data = (Pixel*) t.image.bytes.data();
+	sample.width = sampleSideLength;
+
+	pixels.data = new Pixel[size];
+	pixels.width = sideLength;
+}
+
+Synth::~Synth()
+{
+	delete[] pixels.data;
+}
+
+
+Pixel* PixelBuffer::getPixel(unsigned int x, unsigned int y)
+{
+	if(x >= width || y >= width) {
+		return nullptr;
+	}
+	unsigned int index = (y * width * sizeof(Pixel)) + (x  * sizeof(Pixel));
+	return &data[index];
+}
+
+Pixel* Patch::getPixel(unsigned int x, unsigned int y)
+{
+	if(x >= width || y >= width || sourceImage == nullptr) {
+		return nullptr;
+	}
+	return sourceImage->getPixel(x + offsetX, y + offsetY);
+}
+
+Patch getPatchAround(unsigned int x, unsigned int y, unsigned int patchWidth)
+{
+	return Patch {
+		this,
+		x - patchWidth/2,
+		y - patchWidth/2,
+		patchWidth
+	};
+}
+
+float Patch::difference(Patch& other)
+{
+	if(other.width != width) {
+		cout << "Comparing patches of different sizes!\n";
+		return -1;
+	}
+	float diff = 0;
+	Pixel* pixelA;
+	Pixel* pixelB;
+	for(int x = 0; x < width; x++) {
+		for(int y = 0; y < width, y++) {
+			pixelA = getPixel(x, y);
+			pixelB = other.getPixel(x, y);
+			if(pixelA != nullptr && pixelB != nullptr) {
+				// TODO: DO THE THING
+			}
+		}
+	}
+}
--- src/Texture.h
+++ src/Texture.h
@@ -16,4 +16,42 @@ struct Texture {
 	Texture(const string filepath);
 };
 
+struct Pixel {
+	unsigned char r;
+	unsigned char g;
+	unsigned char b;
+};
+
+struct PixelBuffer {
+	Pixel* data;
+	unsigned int width;
+
+	Pixel* getPixel(unsigned int x, unsigned int y);
+	Patch getPatchAround(unsigned int x, unsigned int y, unsigned int patchWidth);
+};
+
+struct Patch {
+	PixelBuffer* sourceImage;
+	unsigned int offsetX;
+	unsigned int offsetY;
+	unsigned int width;
+	Pixel* getPixel(unsigned int x, unsigned int y);
+	float difference(Patch& other);
+};
+
+class Synth {
+
+	Synth(Texture t);
+	~Synth();
+
+	PixelBuffer sample;
+	PixelBuffer pixels;
+
+	static constexpr int size = 750 * 750;
+	static constexpr int sideLength = 750;
+	static constexpr int sampleSize = 250 * 250;
+	static constexpr int sampleSideLength = 250;
+
+};
+
 #endif
