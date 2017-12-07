99eee606db79cf6301256f87e9d71f52b43a13dd
Kelsey Pownell <kpownell@cs.utexas.edu>
2017-12-07 11:41:33 -0600

    added slider

--- src/GUI.cc
+++ src/GUI.cc
@@ -1,7 +1,19 @@
 #include "GUI.h"
 
+#define SINGLE 0
+
 GUI::GUI(){
 	mainWindow = new Fl_Window(300, 200, "Look at this glorious little window");
+
+        patchSizeSlider = new Fl_Value_Slider(15, 15, 150, 25, "Patch size");
+        patchSizeSlider->type(FL_HOR_NICE_SLIDER);
+        patchSizeSlider->bounds(3, 39);
+        patchSizeSlider->slider_size(0.15);
+        patchSizeSlider->precision(0);
+
+        fileChooser = new Fl_File_Chooser("../assets/", "*", SINGLE, "Texture Sample");
+
+        mainWindow->end();
 	
 }
 
